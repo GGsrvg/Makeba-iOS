7865a33963ed106dc6c49f43c130f1ae7f135003
GGsrvg <x@y.z>
2020-10-15 20:43:06 +0300

    remove generated file

--- makeba/Views/VCStates/NoInternetState.swift
+++ makeba/Views/VCStates/NoInternetState.swift
@@ -9,5 +9,55 @@
 import UIKit
 
 class NoInternetState: UIView {
+    // MARK: - UI Elements
+    lazy var stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var refresh: UIRefreshControl = {
+        let refresh = UIRefreshControl()
+        return refresh
+    }()
+    
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    // MARK: - setup view
+    private func setupView() {
+        
+    }
     
 }
